e7d2e943422c96da203baf416ae3f5fd764e4494
Aicyra <182109647+Aicyraa@users.noreply.github.com>
2026-01-03 19:23:01 +0800

    validation

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
 document.querySelector("button").addEventListener("click", (e) => {
    const inputs = document.querySelectorAll(".inputs");
-   console.log(inputs);
 
    inputs.forEach((el) => {
       let value;
       if (el.id == "fname") {
          value = el.value;
          if (value < 1) validate(false, el, "*Length Must Be more than 1");
+         else validate(true, el);
       } else if (el.id == "lname") {
          value = el.value;
          if (value < 1) validate(false, el, "*Length Must Be more than 1");
+         else validate(true, el);
       } else if (el.id == "age") {
          value = el.value;
-         if (value < 1) validate(false, el, "*You are to young!");
+         if (value == "") validate(false, el, "*Age is required.");
+         else if (value < 1) validate(false, el, "*You are to young!");
          else if (value > 100)
             validate(false, el, "*You are too old to come abord");
+         else validate(true, el);
       } else if (el.id == "email") {
+         value = el.value;
+         if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}/.test(value))
+            validate(false, el, "*Invalid Email!");
+         else validate(true, el);
       } else if (el.id == "number") {
          value = el.value;
-         if (value < 1 && value > 12) validate(false, el, "*Invalid Number!");
+         if (value < 1 || value > 12) validate(false, el, "*Invalid Number!");
+         else validate(true, el);
       }
    });
 });
@@ -29,11 +37,13 @@ function validate(isValid, element, msg) {
    const span = parent.querySelector("#message");
    
    if (!isValid) {
+      input.classList.remove("valid")
       span.classList.add("invalid");
       input.classList.add("invalid");
       span.textContent = msg;
       return;
    } else {
+      input.classList.remove("invalid")
       input.classList.add("valid");
       span.textContent = "";
    }
